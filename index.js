e4d1635c0551fa32ca50d6e92ed3a2be7c32adaa
wahi90 <mdshahed1098@gmail.com>
2022-05-09 21:20:20 +0600

    create inventory data

---
+++ index.js
@@ -0,0 +1,44 @@
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const port = process.env.PORT || 5000;
+const app = express();
+
+// middleware
+
+app.use(cors());
+app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ro8id.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+console.log('MONGODB CONNETED33')
+async function run(){
+    try{
+      await client.connect();
+      const productCollection = client.db('warehouse').collection('product');
+      app.get('/inventory', async(req,res) =>{
+       const query = {};
+      const cursor = productCollection.find(query);
+      const products = await cursor.toArray();
+      res.send(products);
+      })
+     
+    }
+    finally{
+
+    }
+}
+run().catch(console.dir);
+
+
+
+app.get('/' , (req , res) => {
+    res.send('hello iam sahed');
+});
+
+app.listen(port, () => {
+    console.log('listening to port');
+})
